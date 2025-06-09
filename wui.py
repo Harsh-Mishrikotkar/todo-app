011ed1e9f043c78dffeae8df0d55907de7e10737
HarshMishrikotkar <harshmishrikotkar@gmail.com>
2025-06-09 15:50:29 -0500

    Implement add todo feature into the wui

--- wui.py
+++ wui.py
@@ -3,6 +3,12 @@ import functions
 
 todos = functions.getTodos()
 
+def addTodo():
+    todo = st.session_state["newTodo"] + "\n"
+    todos.append(todo)
+    functions.writeTodos(todos)
+
+
 st.title("My ToDo App")
 st.subheader("This is my Todo app")
 st.write("This app is to increase your productivity")
@@ -10,4 +16,7 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo:", placeholder="Add a new ToDo...")
+st.text_input(label="",
+              placeholder="Add a new ToDo...",
+              on_change=addTodo,
+              key="newTodo")
