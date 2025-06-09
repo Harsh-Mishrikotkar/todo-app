2e0be9818ef865a61c3257cb01cae0383bb6d127
HarshMishrikotkar <harshmishrikotkar@gmail.com>
2025-06-09 16:02:13 -0500

    Finished wui

--- wui.py
+++ wui.py
@@ -13,8 +13,13 @@ st.title("My ToDo App")
 st.subheader("This is my Todo app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.writeTodos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="",
               placeholder="Add a new ToDo...",
